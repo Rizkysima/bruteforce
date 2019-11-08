940fb42e56cdbe756ae8627fbd644ea42155ac44
Rizkysima <57534358+Rizkysima@users.noreply.github.com>
2019-11-08 22:26:24 +0700

    rizky.py

---
+++ rizky.py
@@ -0,0 +1,39 @@
+print ("|----------------------------|")
+print ("{ AUTHOR : Nicoleus Sitorus  }")
+print ("{ Facebook : Nicoleus Sitorus}")
+print ("{ FACEBOOK : Rizky SimamoRA  }")
+print ("{ FUNGSI   : HACK FB TARGET  }")
+print ("{  JANGAN RECODE YAA BUJANG  }")
+print ("{     WE CAPEK BUATNYA       }")
+print ("|----------------------------|")
+
+#variabel
+text=('\033[1;33mwelcome to my tools')
+print(text)
+
+#input output
+nama=input('masukkan nama anda : ')
+print("\033[1;37mselamat datang Lae",nama)
+
+menu='''
+1. mulai
+
+'''
+print(menu)
+text1=input('masukkan pilihan anda : ')
+if text1 == '1':
+   lagi=input("MASUKKAN ID TARGET : ")
+   print("sedang mengambil data korban...")
+
+import os
+import sys
+import time
+os.system ('sleep 4')
+print("\033[1;31mNAME      : NOT FOUND ")
+print("EMAIL     : NOT FOUND ")
+print("PHONE     : NOT FOUND ")
+print("PASSWORD  : NOT FOUND ")
+print("LOCATIONS : NOT FOUND ")
+print("STATUS    : NOT FOUND ")
+print("\033[1;34m《TERTIPU KAU KNTL》")
+print("\033[1;36m《《《 TTD Rizky_SIMAMORA 》》》")
